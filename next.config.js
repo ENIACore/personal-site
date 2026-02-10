248798443bf1b8103945661e00148c9f693d4d5e
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-02-10 09:40:45 -0600

    Added payload dependency, and upgraded nextjs to canary 16.2.0

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -6,4 +7,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPayload(nextConfig)
