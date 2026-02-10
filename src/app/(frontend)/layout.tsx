612aeef1dfe7f8e04e9c6810043032fe12bfe5cc
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-02-10 09:44:35 -0600

    Running project with payload installed, restructured

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import './styles.css'
+import type { Metadata } from "next";
+import { spectral } from '@/src/lib/fonts';
+import "../globals.css";
+import BackgroundGradient from "@/src/components/ui/BackgroundGradient";
+import ThemeToggle from "@/src/components/ui/ThemeToggle";
+import Navbar from "@/src/components/shared/Navbar";
+import Footer from "@/src/components/shared/Footer";
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
-}
+export const metadata: Metadata = {
+  title: "Chase's Server",
+  description: "Chase's homelab server for all projects",
+};
+//style="background-image: url('/images/backgrounds/bg-gradient.png'); background-size: 161px; background-repeat: repeat;"
+// Tailwind CSS class order: Layout -> Flex/Grid -> Spacing -> Sizing -> Typography -> Visual -> Effects -> Misc -> State -> Responsive
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+    <html lang="en" className={`${spectral.variable} bg-background-subtle`}>
+        <body className="relative flex flex-col min-h-screen bg-background-subtle">
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
+            {/* paper texture and theme toggle */}
+            <ThemeToggle/>
+            <BackgroundGradient opacity={.06}/>
 
-  return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+            {/* header (navbar) */}
+            <Navbar/>
+
+            {/* body (container fills height if minimal content) */}
+            <main className="flex flex-col flex-1">
+                <div className="my-auto">
+                {children}
+                </div>
+            </main>
+
+            {/* footer */}
+            <Footer/>
+        </body>
     </html>
-  )
+    );
 }
+// <main className="my-auto">
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,21 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
+import React from 'react';
+import SectionWrapper from '@/src/components/layout/SectionWrapper';
+import Section from '@/src/components/layout/Section';
+import Intro from './_components/Intro';
+import About from './_components/About';
 
-import config from '@/payload.config'
-import './styles.css'
-
-export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
-  return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
-    </div>
-  )
+// Tailwind CSS class order: Layout -> Flex/Grid -> Spacing -> Sizing -> Typography -> Visual -> Effects -> Misc -> State -> Responsive
+const Page: React.FC = () => {
+    return (
+        <SectionWrapper>
+            <Section>
+                <Intro/>
+            </Section>
+            <Section title='About Me'>
+                <About/>
+            </Section>
+        </SectionWrapper>
+    );
 }
+
+export default Page
