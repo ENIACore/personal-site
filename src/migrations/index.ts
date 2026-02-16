fbc1dfb8030beabeb7ab0703b749cfa3c0568558
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-02-15 18:24:58 -0600

    Created new initial migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260215_201306 from './20260215_201306';
+import * as migration_20260216_002430_init from './20260216_002430_init';
 
 export const migrations = [
   {
-    up: migration_20260215_201306.up,
-    down: migration_20260215_201306.down,
-    name: '20260215_201306'
+    up: migration_20260216_002430_init.up,
+    down: migration_20260216_002430_init.down,
+    name: '20260216_002430_init'
   },
 ];
